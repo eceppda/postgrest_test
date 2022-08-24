06e45edc37ee95e96ebe42eb7a7c964a9fbd417b
Jeff <jeff@hemminger.haus>
2022-08-24 08:14:47 -0400

    Update 01.sql

--- init/01.sql
+++ init/01.sql
@@ -6,10 +6,6 @@ create role web_anon nologin;
 
 grant usage on schema test to web_anon;
 
-create role authenticator noinherit login password 'ydab2d';
-grant web_anon to authenticator;
-
-
 CREATE FUNCTION test.get_headers()
 RETURNS json AS $$
     SELECT current_setting('request.headers', true)::json;
